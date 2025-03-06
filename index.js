0c4a89e0fbc49ae1b9472925c2736b23befc3ebe
akshay reddy <akshayreddy1906@gmail.com>
2025-03-06 21:43:08 +0530

    storing data in mongodb

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { initializeDatabase } = require("./db/db.connect");
-const fs = require("fs");
+// const fs = require("fs");
 const Movie = require("./models/movie.models");
 
 initializeDatabase();
 
-const jsonData = fs.readFileSync("movies.json");
-const moviesData = JSON.parse(jsonData);
+// const jsonData = fs.readFileSync("movies.json");
+// const moviesData = JSON.parse(jsonData);
 
 // function seedData() {
 //   try {
@@ -31,4 +31,33 @@ const moviesData = JSON.parse(jsonData);
 //   }
 // }
 
-seedData();
+// seedData()
+
+const newMovie = {
+  title: "Salaar",
+  releaseYear: 2023,
+  genre: ["Action", "Thriller"],
+  director: "Prashanth Neel",
+  actors: ["Prabhas", "Shruti Haasan"],
+  language: "Telugu",
+  country: "India",
+  rating: 9.1,
+  plot: "In the crime-infested Khansaar, Prince Varadha sets to ascend the throne. But a coup d'état is planned! And there is only one man to help reclaim power: Deva.",
+  awards: "National Film Award",
+  posterUrl: "https://example.com/salaar.jpg",
+  trailerUrl: "https://example.com/salaar.mp4",
+};
+
+
+
+async function createMovie(newMovie) {
+  try {
+    const movie = new Movie(newMovie);
+    const savedMovie = await movie.save();
+    console.log("Movie saved successfully:", savedMovie);
+  } catch (error) {
+    console.error("Error creating movie:", error);
+  }
+}
+
+createMovie(newMovie);
